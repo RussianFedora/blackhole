9a6cb919740f737ee97cc473f3fe91a27ea79a29
Tigro <ashejn@gmail.com>
2017-01-12 14:20:58 +0300

    fix script for el7

--- get_sources.sh
+++ get_sources.sh
@@ -6,5 +6,6 @@ VERSION=$(rpm -q --specfile *.spec --qf "%{version}\n" | head -n1)
 rm -rf $NAME-$VERSION
 git clone -b v0.2 https://github.com/3Hren/$NAME.git $NAME-$VERSION --depth 1
 
-tar -ca --exclude-vcs --exclude-vcs-ignores -f $NAME-$VERSION.tar.xz $NAME-$VERSION
+rm -rf $NAME-$VERSION/.git
+tar -caf $NAME-$VERSION.tar.xz $NAME-$VERSION
 rm -rf $NAME-$VERSION
